251d68e924ebba4f66e448a3b83f235ef55a691a
kamei <leokamei1@gmail.com>
2025-03-21 10:35:39 -0300

    fixing fuse_ops, now working fine!

--- pytorch/fuse_ops/fuse_ops.py
+++ pytorch/fuse_ops/fuse_ops.py
@@ -2,6 +2,31 @@ import torch
 from torch._C import _jit_pass_dce
 from typing import List
 
+def add(op1, op2):
+    return op1 + op2
+
+def mul(op1, op2):
+    return op1 * op2
+
+def tensor_constant_op(graph: torch._C.Graph, nodes, current, next_node, op):
+    if(op == add):
+        current_input1, current_input2, current_alpha = current.inputs()
+        next_input1, next_input2, next_alpha = next_node.inputs()
+    else:
+        current_input1, current_input2 = current.inputs()
+        next_input1, next_input2 = next_node.inputs()
+
+    if current_input2.node().kind() == "prim::Constant" and next_input2.node().kind() == "prim::Constant":
+        if next_input1 == current.output():
+            value1 = current_input2.toIValue()
+            value2 = next_input2.toIValue()
+            new_int = graph.insertConstant(op(value1, value2))
+            new_int.node().moveBefore(nodes[0])
+            if(op == add): new_op = graph.create("aten::add", [current_input1, new_int, current_alpha])
+            else: new_op = graph.create("aten::mul", [current_input1, new_int])
+            new_op.insertBefore(current)
+            next_node.output().replaceAllUsesWith(new_op.output())
+
 def fuse_consecutive_ops(graph: torch._C.Graph):
     """
     Otimização para fundir operações consecutivas de soma (aten::add).
@@ -14,25 +39,20 @@ def fuse_consecutive_ops(graph: torch._C.Graph):
             current = nodes[i - 1]
             next_node = nodes[i]
             if current.kind() == "aten::add" and next_node.kind() == "aten::add":
-                current_input1, current_input2, current_alpha = current.inputs()
-                next_input1, next_input2, next_alpha = next_node.inputs()
-                if next_input1 == current.output():
-                    value1 = current_input2.toIValue()
-                    value2 = next_input2.toIValue()
-                    new_int = graph.insertConstant(value1 + value2)
-                    new_int.node().moveBefore(nodes[0])
-                    new_add = graph.create("aten::add", [current_input1, new_int, current_alpha])
-                    new_add.insertBefore(current)
-
-                    next_node.output().replaceAllUsesWith(new_add.output())
-                    
-                    del nodes[i]
-                    del nodes[i - 1]
-                    next_node.destroy()
-                    current.destroy()
-                    
-                    nodes = list(graph.nodes())
-                    changes = True
+                tensor_constant_op(graph, nodes, current, next_node, add)
+                del nodes[i]
+                del nodes[i - 1]
+                next_node.destroy()
+                current.destroy()
+                changes = True
+            elif current.kind() == "aten::mul" and next_node.kind() == "aten::mul":
+                tensor_constant_op(graph, nodes, current, next_node, mul)
+                del nodes[i]
+                del nodes[i - 1]
+                next_node.destroy()
+                current.destroy()
+                changes = True
+        nodes = list(graph.nodes())
         print(graph)
     return graph
 
@@ -41,7 +61,6 @@ def destroy_useless_variables(graph: torch._C.Graph):
     for current in nodes[:]:
         if not current.hasUses():
             current.destroy()
-
     return graph
 
 def optimize_script(script_module: torch.jit.ScriptModule) -> torch.jit.ScriptModule:
@@ -51,37 +70,24 @@ def optimize_script(script_module: torch.jit.ScriptModule) -> torch.jit.ScriptMo
 
     return script_module
 
-@torch.jit.script
-def example_function(x: torch.Tensor):
-    a = x + 2 + 4
-    b = a + 3
-    c = torch.relu(b)
-    return c
-fn = torch.jit.script(example_function)
-print(fn.graph)
-optimized_fn = optimize_script(torch.jit.script(example_function))
-print("Funcao otimizada:")
-print(optimized_fn.graph)
-
-x = torch.tensor([1.0])
-output = optimized_fn(x)
-print(output)
-
-
-# class ExampleModule(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.value = torch.ones(1)
+class ExampleModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.value = torch.ones(1)
     
-#     def forward(self, x: torch.Tensor):
-#         a = x + 2 + 4
-#         b = a + 3
-#         c = torch.relu(b)
-#         return c
+    def forward(self, x: torch.Tensor):
+        b = 2 + x
+        d = 4 + b
+        e = d * 5
+        f = e * 5
+        c = torch.relu(f)
+        return c
 
-# instance = ExampleModule()
-# script = torch.jit.script(instance)
-# optimized_module = optimize_script(script)
+instance = ExampleModule()
+script = torch.jit.script(instance)
+print("Classe original:")
+print(script.graph)
+optimized_module = optimize_script(script)
 
-# print("Classe otimizada:")
-# print(optimized_module.graph)
+print("Classe otimizada:")
+print(optimized_module.graph)
